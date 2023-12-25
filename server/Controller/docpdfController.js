134f0c66f6bbed8bebc0266c1ccf3f8a738014a6
Tú BÙI <anhturoyal@gmail.com>
2023-12-25 11:53:58 +0700

    pdffile

---
+++ server/Controller/docpdfController.js
@@ -0,0 +1,61 @@
+const docpdfModal = require("../Modal/docpdfModal");
+const path = require("path");
+
+const customerController = {
+  adddocpdf: async (req, res) => {
+    let newData = {};
+    try {
+      if (req.file) {
+        const filePath = req.file.path;
+        const fileUrl = `http://localhost:${
+          process.env.PORT
+        }/uploads/${path.basename(filePath)}`;
+        newData.fileurl = fileUrl;
+      }
+      const addfile = await docpdfModal.create(newData);
+      res.status(200).json({ message: "Thêm thành công", data: addfile });
+    } catch (error) {
+      res.status(500).json({ message: error.message });
+    }
+  },
+
+  patchdocpdf: async (req, res) => {
+    const { fileId } = req.params;
+    try {
+      if (req.file) {
+        const filePath = req.file.path;
+        const fileUrl = `http://localhost:${
+          process.env.PORT
+        }/uploads/${path.basename(filePath)}`;
+        newData.fileurl = fileUrl;
+      }
+      const updateFile = await docpdfModal.updateOne({ _id: fileId }, newData);
+      res
+        .status(200)
+        .json({ message: "Cập nhật thành công", data: updateFile });
+    } catch (error) {
+      res.status(500).json({ message: error.message });
+    }
+  },
+
+  deletedocpdf: async (req, res) => {
+    const { fileId } = req.params;
+    try {
+      const delefile = await docpdfModal.deleteOne({ _id: fileId });
+      res.status(200).json({ message: "Xóa thành công", data: delefile });
+    } catch (error) {
+      res.status(500).json({ message: error.message });
+    }
+  },
+
+  getdocpdf: async (req, res) => {
+    try {
+      const docpdf = await docpdfModal.find();
+      res.status(200).json({ message: "Thành công", data: docpdf });
+    } catch (error) {
+      res.status(500).json({ message: error.message });
+    }
+  },
+};
+
+module.exports = customerController;
