21051c9b6818d13ad26a364e0c70975393d7e884
Tú BÙI <anhturoyal@gmail.com>
2023-12-29 13:24:24 +0700

    asdasd

--- server/Modal/docpdfModal.js
+++ server/Modal/docpdfModal.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 
-mongoose.connect("mongodb://127.0.0.1:27017/Camera");
+mongoose.connect(process.env.DATA_BASE_URL + process.env.REPOSITORY);
 
 const docpdfSchema = mongoose.Schema(
   {
